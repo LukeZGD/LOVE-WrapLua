b737cdddc043cdfdd1c3559f65fc6ccf8dfb70b1
LukeeGD <l32luke@gmail.com>
2018-10-10 19:10:36 +0800

    some psp stuff

--- LOVE-OneLua/filesystem.lua
+++ LOVE-OneLua/filesystem.lua
@@ -1,49 +1,35 @@
 local saveloc = "ux0:/data/"..appname.."/savedata/"
-if not isPSP and not files.exists(saveloc) then
+if isPSP then
+	saveloc = "ms0:/"..appname.."/savedata/"
+end
+
+if not files.exists(saveloc) then
 	files.mkdir(saveloc)
 end
 
 function love.filesystem.read(file)
-	if not isPSP then
-		local openfile = io.open(saveloc..file, "r")
-		local contents = openfile:read()
-		io.close(openfile)
-		return contents
-	else
-		return data.load(file,1)	
-	end
+	local openfile = io.open(saveloc..file, "r")
+	local contents = openfile:read()
+	io.close(openfile)
+	return contents
 end
 
 function love.filesystem.write(file,datawrite)
-	if not isPSP then
-		local openfile = io.open(saveloc..file, "w+")
-		openfile:write(datawrite)
-		io.close(openfile)
-	else
-		data.save(datawrite,appname,file,file,"",1,"ICONOS/")
-	end
+	local openfile = io.open(saveloc..file, "w+")
+	openfile:write(datawrite)
+	io.close(openfile)
 end
 
 function love.filesystem.remove(file)
-	if not isPSP then
-		files.delete(saveloc..file)
-	else
-		data.delete(file)
-	end
+	files.delete(saveloc..file)
 end
 
 function love.filesystem.load(file)
-	if not isPSP then
-		dofile(saveloc..file)
-	end
+	dofile(saveloc..file)
 end
 
 function love.filesystem.isFile(file)
-	if not isPSP then
-		return files.exists(saveloc..file)
-	else
-		return data.load(file,1)
-	end
+	return files.exists(saveloc..file)
 end
 
 function love.filesystem.getInfo(file)
